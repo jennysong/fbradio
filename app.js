92c4753205363e5e2a86cdbe6822ce258a8754de
Jenny <jenny.jieun.song@gmail.com>
2015-11-19 15:04:43 -0800

    ejs install

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+
 var express = require('express');
 var app = express();
+
+
+global.config = require(__dirname + '/config.json');
+
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
 app.get('/', function (req, res) {
-  res.send('Working!');
+  res.render('index');
 });
 
+app.get('/login', function (req, res) {
+  scopes = ['public_profile', 'user_photos']
+  res.redirect('https://www.facebook.com/dialog/oauth?client_id='+config.client_id+'&scope='+ scopes.join('+') +'&redirect_uri='+config.redirect_uri)
+});
+
+app.get('/authorize', function (req, res) {
+  query = "client_id=" + config.client_id + "&redirect_uri=" + config.redirect_uri + "&client_secret=" + config.client_secret + "&code=" + req.query.code
+  rest.get("https://graph.facebook.com/v2.3/oauth/access_token?" + query, function(data, response){
+    data = JSON.parse(data.toString())
+    req.session.access_token = data.access_token
+    res.redirect('/loading')
+  })
+});
 
 
 var server = app.listen(3000, function () {
