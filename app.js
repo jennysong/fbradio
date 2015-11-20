ee3d2e7a83b9a705af0a84f4e5d2cc831ad49057
Jenny <jenny.jieun.song@gmail.com>
2015-11-19 17:48:18 -0800

    :)

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var env = process.env.NODE_ENV || "development";
 
 var express = require('express');
+var session = require('express-session')
+var file_store = require('session-file-store')(session)
+
 var app = express();
 
 global.config = require(__dirname + '/config.json')[env];
+global.rest   = new (require('node-rest-client').Client);
 
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use('/assets', express.static('assets'));
+app.use(session({
+  store: new file_store(),
+  secret: 'Uv8}4{}h&46U*RQ3',
+  cookie: { maxAge: 60000 }
+}))
 
 
 app.get('/', function (req, res) {
@@ -16,19 +25,17 @@ app.get('/', function (req, res) {
 });
 
 app.get('/login', function (req, res) {
-  scopes = ['public_profile'] //read_stream
+  scopes = ['public_profile', 'user_posts'] 
   res.redirect('https://www.facebook.com/dialog/oauth?client_id='+config.client_id+'&scope='+ scopes.join('+') +'&redirect_uri='+config.redirect_uri)
 });
 
-
-
 app.get('/authorize', function (req, res) {
-  console.log(res);
   query = "client_id=" + config.client_id + "&redirect_uri=" + config.redirect_uri + "&client_secret=" + config.client_secret + "&code=" + req.query.code
   rest.get("https://graph.facebook.com/v2.3/oauth/access_token?" + query, function(data, response){
     data = JSON.parse(data.toString())
     req.session.access_token = data.access_token
-    res.redirect('/index')
+    console.log(req.session.access_token+" :)")
+    res.send("done")  //res.redirect('radio')
   })
 });
 
